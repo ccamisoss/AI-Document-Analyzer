e9c5abdb1c126373a131b513ed6864d81f5805f4
Camila <sosacamila468@gmail.com>
2026-01-25 20:36:36 -0300

    Refactor: Remove documents controller and routes as part of module restructuring

--- backend/src/modules/documents/documents.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { authenticate } from "../auth/auth.middleware.js";
-import { createDocument } from "./documents.controller.js";
-
-const documentsRouter = Router();
-
-documentsRouter.post("/", authenticate, createDocument);
-
-export default documentsRouter;
